5027e1cd8498e8b2b1690c90dbbdfec45a41c87c
jaimevp54 <jaimevp54@gmail.com>
2017-12-03 13:41:22 -0400

    a commit just in case

--- api/views.py
+++ api/views.py
@@ -9,6 +9,7 @@ from django.core import serializers
 from home.models import Station, Sensor, SensorReading
 from django.http import HttpResponse, JsonResponse
 
+from pprint import pprint
 import json
 
 
@@ -47,15 +48,40 @@ class NewReadingsBundle(View):
     def post(self, request):
         body_unicode = request.body.decode('utf-8')
         data = json.loads(body_unicode)
-        for sensor, readings in data.items():
-            for reading in readings:
-                SensorReading.objects.create(
-                    sensor=Sensor.objects.get(name=sensor),
-                    data=float(reading['value']),
-                    date=datetime.strptime(reading['date'], '%Y-%m-%dT%H:%M:%SZ')
-                )
+        pprint(data)
 
-        return HttpResponse("Data received succesfully ->" + json.dumps(data))
+        if Station.objects.filter(station_id=data["station_id"]).exists():
+            station = Station.objects.get(_id=data["station_id"])
+            station.location_altitude = data["location"]["altitude"]
+            station.location_longitude = data["location"]["longitude"]
+            station.location_latitude = data["location"]["latitude"]
+        else:
+            return HttpResponse(f"Station {data['station_id']} is not registered on Playweather Web.\n  ignored.")
+
+        received = []
+        ignored = []
+        for sensor, readings in data['readings'].items():
+            if Sensor.objects.filter(sensor_id=sensor).exists():  # TODO check if this sensor belongs to the right station
+                received.append(sensor)
+                for reading in readings:
+                    SensorReading.objects.create(
+                        sensor=Sensor.objects.get(sensor_id=sensor),
+                        data=float(reading['value']),
+                        date=datetime.strptime(reading['date'], '%Y-%m-%dT%H:%M:%SZ')
+                    )
+            else:
+                ignored.append(sensor)
+
+        response = ""
+        response += "The following sensors are not registered, therefore have been ignorored: \n"
+        for sensor in ignored:
+            response += f" - {sensor}\n"
+        response += "Please register ignored sensors on Playweather Web Admin site (http://playweather.tk/admin) \n\n"
+
+        response += "Data received succesfully for sensors:\n"
+        for sensor in received:
+            response += f" - {sensor}\n"
+        return HttpResponse(response)
 
 
 class ReadReading(View):
@@ -66,6 +92,6 @@ class ReadReading(View):
 
         response = []
         for reading in list(readings.values('id', 'sensor', 'data', 'date')):
-            reading['sensor_name'] = Sensor.objects.get(pk=reading['sensor']).name
+            reading['sensor_id'] = Sensor.objects.get(pk=reading['sensor']).name
             response.append(reading)
         return JsonResponse(response, safe=False)
--- home/models.py
+++ home/models.py
@@ -6,12 +6,13 @@ from django.contrib.auth.models import User
 
 class Station(models.Model):
     owner = models.ForeignKey(User)
-    name = models.CharField(max_length=255)
-    city = models.ForeignKey("cities_light.City", null=True, on_delete=models.SET_NULL)
-    location_longitude = models.FloatField()
-    location_latitude = models.FloatField()
-    location_altitude = models.FloatField()
-    is_active = models.BooleanField()
+    id = models.CharField(primary_key=True, max_length=255)  # TODO must be unique
+    name = models.CharField(max_length=255, unique=True)  # TODO must be unique
+    # city = models.ForeignKey("cities_light.City", null=True, on_delete=models.SET_NULL)
+    location_longitude = models.FloatField(null=True, editable=False)  # TODO can be null
+    location_latitude = models.FloatField(null=True, editable=False)  # TODO can be null
+    location_altitude = models.FloatField(null=True, editable=False)  # TODO can be null
+    is_active = models.BooleanField(editable=False, default=False)
     date_registered = models.DateTimeField(auto_now=True)
 
     @property
@@ -23,22 +24,25 @@ class Station(models.Model):
 
 
 class Sensor(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField()
+    id = models.CharField(primary_key=True, max_length=255)
+    description = models.TextField(blank=True)  # TODO can be null
     type = models.CharField(max_length=255, choices=[
         ('P', 'Pluvial'),
         ('WS', 'Velocidad Viento'),
-        ('WD', 'Direccion Viento'),
-        ('UV', 'Luz Ultra Violeta)'),
+        ('WD', 'Dirsensor_ideccion Viento'),
+        ('UV', 'UV'),
+        ('TEMP', 'Temperatura'),
+        ('HUM', 'Humedad'),
         ('CO', 'CO'),
-        ('CO2', "CO2")
+        ('CO2', 'CO2')
+
     ], default='Pluvial')
     station = models.ForeignKey(Station)
-    is_active = models.BooleanField()
+    is_active = models.BooleanField(editable=False, default=False)
     date_registered = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'{self.name} - Installed at: {self.station.name}'
+        return f'{self.id} - Installed at: {self.station.name}'
 
 
 class SensorReading(models.Model):
@@ -47,4 +51,4 @@ class SensorReading(models.Model):
     sensor = models.ForeignKey(Sensor)
 
     def __str__(self):
-        return f'Sensor:{self.sensor.name} at:{self.date}'
+        return f'Sensor:{self.sensor.id} at:{self.date}'
