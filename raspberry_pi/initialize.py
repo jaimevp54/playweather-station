aaa9f6b655d7dd6e44c7ee50c7634a7de36c0779
jaimevp54 <jaimevp54@gmail.com>
2018-02-03 14:13:50 -0400

    [changed] final details before project 1 presentation

--- raspberry_pi/initialize.py
+++ raspberry_pi/initialize.py
@@ -1,19 +1,20 @@
 from playweather_station.core import PlayWeatherStation
 
-from playweather_station.sensors import co, DHT22, lluvia, viento, ccs811
+from playweather_station.sensors import co, DHT22, lluvia, viento, ccs811, UV
 
 pw = PlayWeatherStation("pw_pi")
 pw.delivery_url = "playweather.fwd.wf"
 pw.delivery_port = ""
+
 # Register module classes in here
 # --> pw.register(module.Class)
-
 
 pw.register(co.CO, 'co')
 pw.register(lluvia.Rain, 'pluvial')
 pw.register(DHT22.DHT22, 'DHT22')
 pw.register(viento.Wind, 'viento')
 pw.register(ccs811.CCS811, 'co2')
+pw.register(UV.UV, 'violeta')
 
 try:
     pw.initialize()
--- station_simulator.py
+++ station_simulator.py
@@ -7,6 +7,7 @@ import time
 import json
 import sqlite3
 import os
+from django.utils.encoding import escape_uri_path
 
 
 def send_data(data, host="localhost", port="8000", path="api/sensor_readings_bundle/new/"):
@@ -15,6 +16,7 @@ def send_data(data, host="localhost", port="8000", path="api/sensor_readings_bun
 
         response = requests.post("http://{host}:{port}/{path}".format(host=host, port=port, path=path),
                                  data=json.dumps(data))
+
         if response.status_code == 200:
             print(response.text)
         else:
@@ -23,8 +25,20 @@ def send_data(data, host="localhost", port="8000", path="api/sensor_readings_bun
         print("error:" + str(e) + "\n\n")
 
 
-db_filename = "pw.sqlite3"
-schema_filename = "pw_schema.sql"
+def send_wunderground_data(data, wunderground_id, wunderground_key):
+
+
+    request_url = "http://http://weatherstation.wunderground.com/weatherstation/updateweatherstation.php?action=updateraw&ID={id}&PASSWORD={password}".format(
+        id=wunderground_id, password=wunderground_key)
+
+    request_url += "&dateutc=2017-11-30+19%3A21%3A00"
+    request_url += "&dateutc={date}".format(
+        date=escape_uri_path(reading['date'].replace("T"," ").replace("Z"," ")))
+
+    request_url += "&humidity=12"
+    response = requests.get(request_url)
+    print(response)
+    print(response.text)
 
 
 def init_db():
@@ -64,8 +78,8 @@ while True:
     pw1 = {
         "station_id": "pw1",
         "location": {
-            "latitude": 19.446264,
-            "longitude": -70.683918,
+            "latitude": 19.44107437,
+            "longitude": -70.68268585,
             "altitude": 201.45,
         },
         "readings": {
@@ -83,63 +97,64 @@ while True:
             ],
         }
     }
+    send_wunderground_data()
 
-    persist_data(pw1)
-    send_data(pw1)
-
-    pw2 = {
-        "station_id": "pw2",
-        "location": {
-            "latitude": 19.446359,
-            "longitude": -70.683111,
-            "altitude": 102.45,
-        },
-        "readings": {
-            "pw2_s1": [
-                {"value": random.randrange(200, 220), "date": datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')},
-            ],
-            "pw2_s2": [
-                {"value": random.randrange(80, 1000), "date": datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')},
-            ],
-            "pw2_s3": [
-                {"value": random.randrange(53, 62), "date": datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')},
-            ],
-            "pw2_s4": [
-                {"value": random.randrange(113, 163), "date": datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')},
-            ],
-            "pw2_s5": [
-                {"value": random.randrange(43, 163), "date": datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')},
-            ],
-        }
-    }
-    send_data(pw2)
-
-    pw3 = {
-        "station_id": "pw3",
-        "location": {
-            "latitude": 19.446415,
-            "longitude": -70.684023,
-            "altitude": 201.87,
-        },
-        "readings": {
-            "pw3_s1": [
-                {"value": random.randrange(68, 88), "date": datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')},
-            ],
-            "pw3_s2": [
-                {"value": random.randrange(1430, 1630), "date": datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')},
-            ],
-            "pw3_s3": [
-                {"value": random.randrange(10, 62), "date": datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')},
-            ],
-            "pw3_s4": [
-                {"value": random.randrange(113, 206), "date": datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')},
-            ],
-            "pw3_s5": [
-                {"value": random.randrange(83, 175), "date": datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')},
-            ],
-            "pw3_s6": [
-                {"value": random.randrange(53, 146), "date": datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')},
-            ],
-        }
-    }
+    #
+    # persist_data(pw1)
+    # send_data(pw1)
+    # pw2 = {
+    #     "station_id": "pw2",
+    #     "location": {
+    #         "latitude": 19.446359,
+    #         "longitude": -70.683111,
+    #         "altitude": 102.45,
+    #     },
+    #     "readings": {
+    #         "pw2_s1": [
+    #             {"value": random.randrange(200, 220), "date": datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')},
+    #         ],
+    #         "pw2_s2": [
+    #             {"value": random.randrange(80, 1000), "date": datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')},
+    #         ],
+    #         "pw2_s3": [
+    #             {"value": random.randrange(53, 62), "date": datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')},
+    #         ],
+    #         "pw2_s4": [
+    #             {"value": random.randrange(113, 163), "date": datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')},
+    #         ],
+    #         "pw2_s5": [
+    #             {"value": random.randrange(43, 163), "date": datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')},
+    #         ],
+    #     }
+    # }
+    # send_data(pw2)
+    #
+    # pw3 = {
+    #     "station_id": "pw3",
+    #     "location": {
+    #         "latitude": 19.446415,
+    #         "longitude": -70.684023,
+    #         "altitude": 201.87,
+    #     },
+    #     "readings": {
+    #         "pw3_s1": [
+    #             {"value": random.randrange(68, 88), "date": datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')},
+    #         ],
+    #         "pw3_s2": [
+    #             {"value": random.randrange(1430, 1630), "date": datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')},
+    #         ],
+    #         "pw3_s3": [
+    #             {"value": random.randrange(10, 62), "date": datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')},
+    #         ],
+    #         "pw3_s4": [
+    #             {"value": random.randrange(113, 206), "date": datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')},
+    #         ],
+    #         "pw3_s5": [
+    #             {"value": random.randrange(83, 175), "date": datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')},
+    #         ],
+    #         "pw3_s6": [
+    #             {"value": random.randrange(53, 146), "date": datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')},
+    #         ],
+    #     }
+    # }
     send_data(pw3)
