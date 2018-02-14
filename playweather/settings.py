7b2b2e5664df904353c6b6e6e2957e47ba162ecd
jaimevp54 <jaimevp54@gmail.com>
2018-02-14 15:08:04 -0400

    [added] whitenoise

--- playweather/settings.py
+++ playweather/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,7 +124,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'playweather/static'),
-    os.path.join(BASE_DIR, 'home/static'),
+    os.path.join(BASE_DIR, 'home/static')
 )
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
