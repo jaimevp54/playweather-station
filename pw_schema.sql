7cfa0bfed24069f2ee498378f70e9571819cc7b7
jaimevp54 <jaimevp54@gmail.com>
2018-03-04 14:39:30 -0400

    [added] gps readings to local database

--- pw_schema.sql
+++ pw_schema.sql
@@ -7,3 +7,11 @@ create table readings (
     reading_date  date,
     PRIMARY KEY (sensor, reading_date)
 );
+
+create table gps (
+    latitude  real,
+    longitude real,
+    altitude real,
+    reading_date  date,
+    PRIMARY KEY (reading_date)
+);
